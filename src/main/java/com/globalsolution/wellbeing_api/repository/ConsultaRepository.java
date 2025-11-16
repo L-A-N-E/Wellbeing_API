5adf4e4180ea6d2a8b875c5eb9d1a61226932e97
Alice <alicesb2006@gmail.com>
2025-11-16 17:18:04 -0300

    add: fazendo os repositorios do projeto

---
+++ src/main/java/com/globalsolution/wellbeing_api/repository/ConsultaRepository.java
@@ -0,0 +1,18 @@
+package com.globalsolution.wellbeing_api.repository;
+
+import com.globalsolution.wellbeing_api.domain.model.Consulta;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+/**
+ * Repositório das consultas.
+ */
+public interface ConsultaRepository extends JpaRepository<Consulta, Long> {
+
+    /** Lista consultas de um paciente */
+    List<Consulta> findByPacienteId(Long pacienteId);
+
+    /** Lista consultas de um profissional */
+    List<Consulta> findByProfissionalId(Long profissionalId);
+}
