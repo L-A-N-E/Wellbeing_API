c031c8bf9d9dd57fd496f0548366334d3e2d4489
Alice <alicesb2006@gmail.com>
2025-11-16 13:43:35 -0300

    initial commit

---
+++ src/main/java/com/globalsolution/wellbeing_api/WellbeingApiApplication.java
@@ -0,0 +1,13 @@
+package com.globalsolution.wellbeing_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WellbeingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WellbeingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/globalsolution/wellbeing_api/WellbeingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.globalsolution.wellbeing_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WellbeingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
